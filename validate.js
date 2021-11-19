1c3ecaa2c1d09d7f960e322782bbf42ff96a1975
Shah Harsh <harshshah04042001@gmail.com>
2021-11-20 00:51:53 +0530

    portfolio with preloader

--- validate.js
+++ validate.js
@@ -1,3 +1,10 @@
 function myFunction() {
     alert("Form Submitted and we contact you ASAP");
 }
+
+var preloader = document.getElementById("loading");
+
+function loading(){
+    preloader.style.display = 'none';
+}
+
